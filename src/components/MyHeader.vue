3d5e677fa0f8748bc62a49cdea06364cac4ee797
Jiao1008 <849928058@qq.com>
2022-03-28 11:30:08 +0800

    amend

--- src/components/MyHeader.vue
+++ src/components/MyHeader.vue
@@ -3,6 +3,7 @@
     <input
       type="text"
       placeholder="请输入你的任务名称,按回车键确认"
+      v-model="title"
       @keyup.enter="add"
     />
   </div>
@@ -19,13 +20,13 @@ export default {
     };
   },
   methods: {
-    add(e) {
+    add() {
       // 校验数据
-      // if (!this.title.trim()) return alert("输入不能为空!");
+      if (!this.title.trim()) return alert("输入不能为空!");
       // 将用户的输入包装成为一个 todo 对象
       const todoObj = {
         id: nanoid(),
-        title: e.target.value,
+        title: this.title,
         done: false,
       };
       // 通知 App 组件去添加一个todo对象
